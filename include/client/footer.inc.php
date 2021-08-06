4dca7b432096bbf3710cd6d9d1cf2def2c43464f
youngjinkim817 <58968281+youngjinkim817@users.noreply.github.com>
2021-08-06 17:11:24 +0900

    foot message update
    foot message update

--- include/client/footer.inc.php
+++ include/client/footer.inc.php
@@ -1,7 +1,7 @@
         </div>
     </div>
     <div id="footer">
-        <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'osTicket.com'; ?> - webhoo trigger.</p>
+        <p>Copyright &copy; <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'osTicket.com'; ?> - Jenkins Triggerd Update Demo ~~~</p>
         <a id="poweredBy" href="http://osticket.com" target="_blank"><?php echo __('Helpdesk software - powered by osTicket'); ?></a>
     </div>
 <div id="overlay"></div>
